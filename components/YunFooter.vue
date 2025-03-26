76df45bcbe6beb83f2c76b693084cb0fa89c470e
Kwisma <77215384+Kwisma@users.noreply.github.com>
2025-03-26 23:01:33 +0800

    .

--- components/YunFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <!--音乐播放器-->
-  <meting-js fixed="true" autoplay="true" theme="#409EFF" list-folded="true" server="netease" type="playlist"
-    id="2568697963"></meting-js>
-</template>
--- setup/main.ts
+++
@@ -1,3 +0,0 @@
-import 'https://npm.elemecdn.com/aplayer@1.10.1/dist/APlayer.min.css'
-import 'https://npm.elemecdn.com/aplayer@1.10.1/dist/APlayer.min.js'
-import 'https://npm.elemecdn.com/meting2@0.0.1/js/Meting.min.js'
