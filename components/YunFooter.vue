32bdfa935dc7244c7a6bbe07d4cb9d76b5bfdf30
Kwisma <77215384+Kwisma@users.noreply.github.com>
2025-03-26 22:05:47 +0800

    修复注入

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -1,12 +1,5 @@
-<script lang="ts" setup>
-import 'https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js'
-import 'https://cdn.jsdelivr.net/npm/meting@2/dist/Meting.min.js'
-</script>
 <template>
   <!--音乐播放器-->
   <meting-js fixed="true" autoplay="true" theme="#409EFF" list-folded="true" server="netease" type="playlist"
     id="2568697963"></meting-js>
 </template>
-<style scoped>
-@import url('https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css');
-</style>
